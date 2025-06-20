6f6771b9adb68ccfe54c3e2028492dd4a5ef7eee
Nick Chase <nchase@earthlink.net>
2025-06-20 16:28:18 -0400

    Change to port 80

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def inject_branding():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=80)
